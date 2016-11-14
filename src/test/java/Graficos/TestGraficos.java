ad39f70cf1d80b09ae3029dc789a7fe42562dca9
FrancieliFoppa <francieli.foppa@gmail.com>
2016-11-14 02:33:12 -0200

    v21 inclusão das classes para geração de graficos

---
+++ src/test/java/Graficos/TestGraficos.java
@@ -0,0 +1,18 @@
+package Graficos;
+
+import org.junit.Test;
+
+import br.com.emissoesti.controller.Graficos_Controller;
+
+
+public class TestGraficos {
+
+	@Test
+	public void test() {
+		
+		//Graficos_Controller demo = new Graficos_Controller("Comparação", "Qual sistema operacional você está usando?");
+        //demo.pack();
+        //demo.setVisible(true);
+	}
+
+}
