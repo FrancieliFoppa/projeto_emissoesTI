f0c53c588b63ef36ca7404307821ddff61d7f3b8
FrancieliFoppa <francieli.foppa@gmail.com>
2016-11-16 02:56:39 -0200

    v22.2 - ajuste cálculo - inclusão de totais

--- src/main/java/br/com/emissoesti/DAO/Calculadora_DAO.java
+++ src/main/java/br/com/emissoesti/DAO/Calculadora_DAO.java
@@ -2,12 +2,9 @@ package br.com.emissoesti.DAO;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-import br.com.emissoesti.model.Calculadora;
 
 public class Calculadora_DAO {
 	
-	private Calculadora calc;
-
 	public Calculadora_DAO(Connection connection) throws InstantiationException, IllegalAccessException, ClassNotFoundException {
 		try {
 			connection = new ConnectionBD().getConnection();
@@ -15,21 +12,5 @@ public class Calculadora_DAO {
 			throw new RuntimeException(e);
 		}
 	}
-	/*
-	public void adiciona(Calculadora calc) {
-		String sql = "insert into ativo_ti (nome, valor) values (?,?)"; 
-		PreparedStatement stmt;
-		try {
-			System.out.println("TESTE");
-			   //stmt = connection.prepareStatement(sql); ---> classe CONNECTION
-			//stmt.setString(1, ativo.getHostName());
-			//stmt.setDouble(2, ativo.getConsumoEnergia());
-			//stmt.setDouble(2, ativo.getValorEmissaoCO());
-			//stmt.setString(2, ativo.getFabricante());
-			   //stmt.execute();
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		}
-	}*/
 
 }
--- src/main/java/br/com/emissoesti/controller/Calculadora_Controller.java
+++ src/main/java/br/com/emissoesti/controller/Calculadora_Controller.java
@@ -24,6 +24,169 @@ public class Calculadora_Controller extends Calculadora{
 	}
 	
 	/*
+	 * método calcula o consumo de energia através do consumo por hora do hardware
+	 * multiplicado pela quantidade de dias que permance ligado e pelo consumo de Watts de energia
+	 */
+	@POST	
+	@Consumes(MediaType.TEXT_PLAIN)
+	@Produces(MediaType.TEXT_PLAIN)
+	public ArrayList<AtivoTI> calculaConsumoEnergia(int idUsuario) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException{
+		
+		ArrayList<AtivoTI> listaDeAtivos = new ArrayList<AtivoTI>();
+		AtivoTI_DAO ativoDAO = new AtivoTI_DAO();
+		
+		listaDeAtivos = ativoDAO.listaAtivoInfCalcConsumo(idUsuario);
+		
+		double consumoMegaWatt = 0.0;
+		double resultadoConsumo = 0.0;
+		//percorre a lista de ativos
+		for(int i = 0; i <= (listaDeAtivos.size()-1); i++){
+			
+			//converte o consumo de energia de Watts para MegaWatt
+			consumoMegaWatt = (listaDeAtivos.get(i).getConsumoEnergia() / 1000000); 
+			System.out.println(consumoMegaWatt);
+			//multiplica pelo consumo diario
+			resultadoConsumo = (consumoMegaWatt * listaDeAtivos.get(i).getHorasConsumoDiario());
+			listaDeAtivos.get(i).setConsumoEnergiaDiario(resultadoConsumo);
+			System.out.println(listaDeAtivos.get(i).getIdAtivo() + " Consumo diario: " + listaDeAtivos.get(i).getConsumoEnergiaDiario());
+			
+			//multiplica pelo consumo semanal
+			double HrsconsumoSemanal = 0.0;
+			HrsconsumoSemanal = (listaDeAtivos.get(i).getConsumoEnergiaDiario() * listaDeAtivos.get(i).getDiasConsumo());
+			resultadoConsumo = HrsconsumoSemanal;
+			listaDeAtivos.get(i).setConsumoEnergiaSemanal(resultadoConsumo);
+			System.out.println(listaDeAtivos.get(i).getIdAtivo() + " Consumo semanal: " + listaDeAtivos.get(i).getConsumoEnergiaSemanal());
+			
+			//multiplica pelo consumo mensal
+			double HrsconsumoMensal = 0.0;
+			HrsconsumoMensal = (HrsconsumoSemanal * 4);
+			resultadoConsumo = HrsconsumoMensal;
+			listaDeAtivos.get(i).setConsumoEnergiaMensal(resultadoConsumo);
+			System.out.println(listaDeAtivos.get(i).getIdAtivo() + " Consumo mensal: " + listaDeAtivos.get(i).getConsumoEnergiaMensal());
+			
+			//multiplica pelo consumo anual
+			double HrsconsumoAnual = 0.0;
+			HrsconsumoAnual = (HrsconsumoMensal * 12);
+			resultadoConsumo = HrsconsumoAnual;
+			listaDeAtivos.get(i).setConsumoEnergiaAnual(resultadoConsumo);
+			System.out.println(listaDeAtivos.get(i).getIdAtivo() + " Consumo anual: " + listaDeAtivos.get(i).getConsumoEnergiaAnual());
+			
+		}
+				
+		ativoDAO.atualizaConsumo(listaDeAtivos, idUsuario);
+		return listaDeAtivos;
+		
+	}
+	
+	/*
+	 * método calcula o somatório do consumo de energia diário de cada ativo
+	 */
+	@POST	
+	@Consumes(MediaType.TEXT_PLAIN)
+	@Produces(MediaType.TEXT_PLAIN)
+	public double calculaConsumoDiarioTotal(int idUsuario) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException{
+		
+		ArrayList<AtivoTI> listaDeAtivos = new ArrayList<AtivoTI>();
+		AtivoTI_DAO ativoDAO = new AtivoTI_DAO();
+		
+		listaDeAtivos = ativoDAO.listaAtivoInfConsumo(idUsuario);
+		
+		double consumoDiarioTotal = 0.0;
+		//percorre a lista de ativos
+		for(int i = 0; i <= (listaDeAtivos.size()-1); i++){
+			
+			//somatorio
+			consumoDiarioTotal += listaDeAtivos.get(i).getConsumoEnergiaDiario();
+			System.out.println(consumoDiarioTotal);
+			
+		}
+		
+		return consumoDiarioTotal;
+		
+	}
+	
+	/*
+	 * método calcula o somatório do consumo de energia semanal de cada ativo
+	 */
+	@POST	
+	@Consumes(MediaType.TEXT_PLAIN)
+	@Produces(MediaType.TEXT_PLAIN)
+	public double calculaConsumoSemanalTotal(int idUsuario) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException{
+		
+		ArrayList<AtivoTI> listaDeAtivos = new ArrayList<AtivoTI>();
+		AtivoTI_DAO ativoDAO = new AtivoTI_DAO();
+		
+		listaDeAtivos = ativoDAO.listaAtivoInfConsumo(idUsuario);
+		
+		double consumoSemanalTotal = 0.0;
+		//percorre a lista de ativos
+		for(int i = 0; i <= (listaDeAtivos.size()-1); i++){
+			
+			//somatorio
+			consumoSemanalTotal += listaDeAtivos.get(i).getConsumoEnergiaSemanal();
+			System.out.println(consumoSemanalTotal);
+			
+		}
+		
+		return consumoSemanalTotal;
+		
+	}
+	
+	/*
+	 * método calcula o somatório do consumo de energia mensal de cada ativo
+	 */
+	@POST	
+	@Consumes(MediaType.TEXT_PLAIN)
+	@Produces(MediaType.TEXT_PLAIN)
+	public double calculaConsumoMensalTotal(int idUsuario) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException{
+		
+		ArrayList<AtivoTI> listaDeAtivos = new ArrayList<AtivoTI>();
+		AtivoTI_DAO ativoDAO = new AtivoTI_DAO();
+		
+		listaDeAtivos = ativoDAO.listaAtivoInfConsumo(idUsuario);
+		
+		double consumoMensalTotal = 0.0;
+		//percorre a lista de ativos
+		for(int i = 0; i <= (listaDeAtivos.size()-1); i++){
+			
+			//somatorio
+			consumoMensalTotal += listaDeAtivos.get(i).getConsumoEnergiaMensal();
+			System.out.println(consumoMensalTotal);
+			
+		}
+		
+		return consumoMensalTotal;
+		
+	}
+	
+	/*
+	 * método calcula o somatório do consumo de energia anual de cada ativo
+	 */
+	@POST	
+	@Consumes(MediaType.TEXT_PLAIN)
+	@Produces(MediaType.TEXT_PLAIN)
+	public double calculaConsumoAnualTotal(int idUsuario) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException{
+		
+		ArrayList<AtivoTI> listaDeAtivos = new ArrayList<AtivoTI>();
+		AtivoTI_DAO ativoDAO = new AtivoTI_DAO();
+		
+		listaDeAtivos = ativoDAO.listaAtivoInfConsumo(idUsuario);
+		
+		double consumoAnualTotal = 0.0;
+		//percorre a lista de ativos
+		for(int i = 0; i <= (listaDeAtivos.size()-1); i++){
+			
+			//somatorioo
+			consumoAnualTotal += listaDeAtivos.get(i).getConsumoEnergiaAnual();
+			System.out.println(consumoAnualTotal);
+			
+		}
+		
+		return consumoAnualTotal;
+		
+	}
+	
+	/*
 	 * método calcula a quantidade de CO² emitida atavés do consumo de energia por hardware
 	 * multiplicado pelo fator de emissão de CO².
 	 */
@@ -68,62 +231,110 @@ public class Calculadora_Controller extends Calculadora{
 		return listaDeAtivos;
 		
 	}
-		
+	
 	/*
-	 * método calcula o consumo de energia através do consumo por hora do hardware
-	 * multiplicado pela quantidade de dias que permance ligado e pelo consumo de Watts de energia
+	 * método calcula o somatório das emissões de CO2 diária de cada ativo
 	 */
 	@POST	
 	@Consumes(MediaType.TEXT_PLAIN)
 	@Produces(MediaType.TEXT_PLAIN)
-	public ArrayList<AtivoTI> calculaConsumoEnergia(int idUsuario) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException{
+	public double calculaEmissaoDiarioTotal(int idUsuario) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException{
 		
 		ArrayList<AtivoTI> listaDeAtivos = new ArrayList<AtivoTI>();
 		AtivoTI_DAO ativoDAO = new AtivoTI_DAO();
 		
-		listaDeAtivos = ativoDAO.listaAtivoInfConsumo(idUsuario);
-		
-		double consumoMegaWatt = 0.0;
-		double resultadoConsumo = 0.0;
+		listaDeAtivos = ativoDAO.listaAtivoInfEmissao(idUsuario);
+
+		double emissaoDiarioTotal = 0.0;
 		//percorre a lista de ativos
 		for(int i = 0; i <= (listaDeAtivos.size()-1); i++){
 			
-			//converte o consumo de energia de Watts para MegaWatt
-			consumoMegaWatt = (listaDeAtivos.get(i).getConsumoEnergia() / 1000000); 
-			System.out.println(consumoMegaWatt);
-			//multiplica pelo consumo diario
-			resultadoConsumo = (consumoMegaWatt * listaDeAtivos.get(i).getHorasConsumoDiario());
-			listaDeAtivos.get(i).setConsumoEnergiaDiario(resultadoConsumo);
-			System.out.println(listaDeAtivos.get(i).getIdAtivo() + " Consumo diario: " + listaDeAtivos.get(i).getConsumoEnergiaDiario());
-			
-			//multiplica pelo consumo semanal
-			double HrsconsumoSemanal = 0.0;
-			HrsconsumoSemanal = (listaDeAtivos.get(i).getHorasConsumoDiario() * listaDeAtivos.get(i).getDiasConsumo());
-			resultadoConsumo = (consumoMegaWatt * HrsconsumoSemanal);
-			listaDeAtivos.get(i).setConsumoEnergiaSemanal(resultadoConsumo);
-			System.out.println(listaDeAtivos.get(i).getIdAtivo() + " Consumo semanal: " + listaDeAtivos.get(i).getConsumoEnergiaSemanal());
-			
-			//multiplica pelo consumo mensal
-			double HrsconsumoMensal = 0.0;
-			HrsconsumoMensal = (HrsconsumoSemanal * 4);
-			resultadoConsumo = (consumoMegaWatt * HrsconsumoMensal);
-			listaDeAtivos.get(i).setConsumoEnergiaMensal(resultadoConsumo);
-			System.out.println(listaDeAtivos.get(i).getIdAtivo() + " Consumo mensal: " + listaDeAtivos.get(i).getConsumoEnergiaMensal());
-			
-			//multiplica pelo consumo anual
-			double HrsconsumoAnual = 0.0;
-			HrsconsumoAnual = (HrsconsumoMensal * 12);
-			resultadoConsumo = (consumoMegaWatt * HrsconsumoAnual);
-			listaDeAtivos.get(i).setConsumoEnergiaAnual(resultadoConsumo);
-			System.out.println(listaDeAtivos.get(i).getIdAtivo() + " Consumo anual: " + listaDeAtivos.get(i).getConsumoEnergiaAnual());
+			//somatorio
+			emissaoDiarioTotal += listaDeAtivos.get(i).getValorEmissaoCODiario();
 			
 		}
-				
-		ativoDAO.atualizaConsumo(listaDeAtivos, idUsuario);
-		return listaDeAtivos;
+		
+		return emissaoDiarioTotal;
 		
 	}
 	
+	/*
+	 * método calcula o somatório das emissões de CO2 semanal de cada ativo
+	 */
+	@POST	
+	@Consumes(MediaType.TEXT_PLAIN)
+	@Produces(MediaType.TEXT_PLAIN)
+	public double calculaEmissaoSemanalTotal(int idUsuario) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException{
+		
+		ArrayList<AtivoTI> listaDeAtivos = new ArrayList<AtivoTI>();
+		AtivoTI_DAO ativoDAO = new AtivoTI_DAO();
+		
+		listaDeAtivos = ativoDAO.listaAtivoInfEmissao(idUsuario);
+	
+		double emissaoSemanalTotal = 0.0;
+		//percorre a lista de ativos
+		for(int i = 0; i <= (listaDeAtivos.size()-1); i++){
+			
+			//somatorio
+			emissaoSemanalTotal += listaDeAtivos.get(i).getValorEmissaoCOSemanal();
+			
+		}
+		
+		return emissaoSemanalTotal;
+		
+	}
+	
+	/*
+	 * método calcula o somatório das emissões de CO2 mensal de cada ativo
+	 */
+	@POST	
+	@Consumes(MediaType.TEXT_PLAIN)
+	@Produces(MediaType.TEXT_PLAIN)
+	public double calculaEmissaoMensalTotal(int idUsuario) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException{
+		
+		ArrayList<AtivoTI> listaDeAtivos = new ArrayList<AtivoTI>();
+		AtivoTI_DAO ativoDAO = new AtivoTI_DAO();
+		
+		listaDeAtivos = ativoDAO.listaAtivoInfEmissao(idUsuario);
+
+		double emissaoMensalTotal = 0.0;
+		//percorre a lista de ativos
+		for(int i = 0; i <= (listaDeAtivos.size()-1); i++){
+			
+			//somatorio
+			emissaoMensalTotal += listaDeAtivos.get(i).getValorEmissaoCOMensal();
+			
+		}
+		
+		return emissaoMensalTotal;
+		
+	}
+	
+	/*
+	 * método calcula o somatório do cosumo de energia anual de cada ativo
+	 */
+	@POST	
+	@Consumes(MediaType.TEXT_PLAIN)
+	@Produces(MediaType.TEXT_PLAIN)
+	public double calculaEmissaoAnualTotal(int idUsuario) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException{
+		
+		ArrayList<AtivoTI> listaDeAtivos = new ArrayList<AtivoTI>();
+		AtivoTI_DAO ativoDAO = new AtivoTI_DAO();
+		
+		listaDeAtivos = ativoDAO.listaAtivoInfEmissao(idUsuario);
+
+		double emissaoAnualTotal = 0.0;
+		//percorre a lista de ativos
+		for(int i = 0; i <= (listaDeAtivos.size()-1); i++){
+			
+			//somatorio
+			emissaoAnualTotal += listaDeAtivos.get(i).getValorEmissaoCOAnual();
+			
+		}
+		
+		return emissaoAnualTotal;
+		
+	}
 		
 	/*
 	 * método que calcula o custo de utilização de enegia pelos hardwares, apartir do consumo informado,
